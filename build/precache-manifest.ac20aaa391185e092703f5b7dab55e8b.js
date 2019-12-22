f1c125a822c26a351d307b7606cac7002858ac0e
Athul P <athul929@gmail.com>
2019-12-22 20:30:27 +0530

    add build

---
+++ build/precache-manifest.ac20aaa391185e092703f5b7dab55e8b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c410b711e5d5027a78d3a9971b1020f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "14608bd267ff311b2282",
+    "url": "/static/css/main.e3ba51e9.chunk.css"
+  },
+  {
+    "revision": "8e14b9c9ec8c378887e0",
+    "url": "/static/js/2.b12ef00a.chunk.js"
+  },
+  {
+    "revision": "e2fc3609d47d90b8cc004bfe15ddf15e",
+    "url": "/static/js/2.b12ef00a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "14608bd267ff311b2282",
+    "url": "/static/js/main.5177706c.chunk.js"
+  },
+  {
+    "revision": "c9913191e565de632244",
+    "url": "/static/js/runtime-main.170f8412.js"
+  }
+]);
